9db7ab5b28e4486dc05a4b317e9685e17c86528c
fjorelat <162758042+fjorelat@users.noreply.github.com>
2024-07-21 17:45:24 +0100

    Added the js and imported the moment.js and moment-timezone

--- style/index.js
+++ style/index.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  let athensElement = document.querySelector("#athens");
+  let athensDateElement = athensElement.querySelector(".date");
+  let athensTimeElement = athensElement.querySelector(".time");
+  let athensTime = moment().tz("Europe/Athens");
 
+  athensDateElement.innerHTML = athensTime.format("MMMM Do YYYY");
+  athensTimeElement.innerHTML = athensTime.format(
+    "h:mm:ss:SS [<small>] A [</small>]"
+  );
+
+  let dublinElement = document.querySelector("#dublin");
+  let dublinDateElement = dublinElement.querySelector(".date");
+  let dublinTimeElement = dublinElement.querySelector(".time");
+  let dublinTime = moment().tz("Europe/Dublin");
+
+  dublinDateElement.innerHTML = dublinTime.format("MMMM Do YYYY");
+  dublinTimeElement.innerHTML = dublinTime.format(
+    "h:mm:ss:SS [<small>] A [</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1);
