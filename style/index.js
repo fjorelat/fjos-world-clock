04b0b616a130273306519901b94327afe3f4072b
fjorelat <162758042+fjorelat@users.noreply.github.com>
2024-07-22 21:56:07 +0100

    Added my current location html+js

--- style/index.js
+++ style/index.js
@@ -1,32 +1,39 @@
 function updateTime() {
   let athensElement = document.querySelector("#athens");
-  let athensDateElement = athensElement.querySelector(".date");
-  let athensTimeElement = athensElement.querySelector(".time");
-  let athensTime = moment().tz("Europe/Athens");
+  if (athensElement) {
+    let athensDateElement = athensElement.querySelector(".date");
+    let athensTimeElement = athensElement.querySelector(".time");
+    let athensTime = moment().tz("Europe/Athens");
 
-  athensDateElement.innerHTML = athensTime.format("MMMM Do YYYY");
-  athensTimeElement.innerHTML = athensTime.format(
-    "h:mm:ss:SS [<small>] A [</small>]"
-  );
+    athensDateElement.innerHTML = athensTime.format("MMMM Do YYYY");
+    athensTimeElement.innerHTML = athensTime.format(
+      "h:mm:ss:SS [<small>] A [</small>]"
+    );
+  }
 
   let dublinElement = document.querySelector("#dublin");
-  let dublinDateElement = dublinElement.querySelector(".date");
-  let dublinTimeElement = dublinElement.querySelector(".time");
-  let dublinTime = moment().tz("Europe/Dublin");
+  if (dublinElement) {
+    let dublinDateElement = dublinElement.querySelector(".date");
+    let dublinTimeElement = dublinElement.querySelector(".time");
+    let dublinTime = moment().tz("Europe/Dublin");
 
-  dublinDateElement.innerHTML = dublinTime.format("MMMM Do YYYY");
-  dublinTimeElement.innerHTML = dublinTime.format(
-    "h:mm:ss:SS [<small>] A [</small>]"
-  );
+    dublinDateElement.innerHTML = dublinTime.format("MMMM Do YYYY");
+    dublinTimeElement.innerHTML = dublinTime.format(
+      "h:mm:ss:SS [<small>] A [</small>]"
+    );
+  }
 }
 
 function updateCity(event) {
-  let cityTimezome = event.target.value;
-  let cityName = cityTimezome.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimezome);
+  let cityTimezone = event.target.value;
+  if (cityTimezone === "current") {
+    cityTimezone = moment.tz.guess();
+  }
+  let cityName = cityTimezone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimezone);
   let citiesElement = document.querySelector("#cities");
   citiesElement.innerHTML = `
-    <div class="city>
+    <div class="city">
       <div>
         <h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
